a2e884f9851aed6ca95e35a270a5101bd4163e0f
pavan <pavanbellam06@gmail.com>
2025-01-22 09:57:08 -0800

    Lab 3

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ def double(n:int) -> int:
 Because it is n*n the first test case will pass but the second will not and will return 9 
 so we must change it to n*2 to fix all our test cases. 
 
-You can create as many test cases as you want or none at all depending on what you need to solve your problem
+No the code would not have been correct since one test case would have failed, you can create as many test cases as you want to make sure your code works
 '''
 
 
