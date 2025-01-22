a485e5243f3cb07cfaf15494e9ec93843b67ca89
pavan <pavanbellam06@gmail.com>
2025-01-22 09:55:06 -0800

    Lab 3

--- functions.py
+++ functions.py
@@ -2,5 +2,13 @@
 # Input: a number to be doubled
 # Result: a number
 def double(n:int) -> int:
-    result = n * n
+    result = n * 2
     return result
+'''
+Because it is n*n the first test case will pass but the second will not and will return 9 
+so we must change it to n*2 to fix all our test cases. 
+
+You can create as many test cases as you want or none at all depending on what you need to solve your problem
+'''
+
+
